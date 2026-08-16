fe42bcfb7e59ca5f690fb5ceff829d1481a694c1
haroldapaza <40350554+haroldapaza@users.noreply.github.com>
2026-08-16 18:21:11 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://ren.fama.net.pe",
+        "https://webren.fama.net.pe",
     ],
     allow_credentials=True,
     allow_methods=["*"],
